d8a8ff3a6a062691eae20c092387f47b834b0d4b
Eli Forbes <eliforbes42@gmail.com>
2020-10-01 14:56:08 -0700

    Document Menu Button

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -1,3 +1,6 @@
+import { useConst } from "@uifabric/react-hooks";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+import { IContextualMenuProps } from "office-ui-fabric-react/lib/ContextualMenu";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { navRoutes, navStrings } from "../../Strings";
@@ -15,12 +18,25 @@ const Header = (props: Props) => {
     </div>
   ));
 
+  const documentStrings = ["Resume", "Transcript", "Cover Letter", "Letter of Recommendation"];
+
+  const menuProps = useConst<IContextualMenuProps>(() => ({
+    shouldFocusOnMount: true,
+    items: documentStrings.map((document) => ({
+      key: document.toLowerCase().replace(/\s/g, "_"),
+      text: document,
+      href: `./${document.toLowerCase().replace(/\s/g, "_")}.pdf`,
+    })),
+  }));
+
   return (
     <header className="app-header">
       <Link to="/" className="logo-nav-item">
         <img src="./CatamaranLogo.png" className={"nav-logo"} alt={"Eli Forbes"} />
       </Link>
-      <div className="nav-align-right">{navItemsInternal}</div>
+      <div className="document-menu-button">
+        <DefaultButton text={"Documents"} menuProps={menuProps} />
+      </div>
     </header>
   );
 };
