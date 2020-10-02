4e76521dedac92744e39b476f1c358d453c2cf85
Eli Forbes <eliforbes42@gmail.com>
2020-10-02 11:00:54 -0700

    Updated Color Scheme

--- src/components/Main/Header.tsx
+++ src/components/Main/Header.tsx
@@ -1,25 +1,13 @@
 import { useConst } from "@uifabric/react-hooks";
 import { DefaultButton } from "office-ui-fabric-react/lib/Button";
 import { IContextualMenuProps } from "office-ui-fabric-react/lib/ContextualMenu";
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { navRoutes, navStrings } from "../../Strings";
+import { documentStrings } from "../../Strings";
 
 interface Props {}
 
 const Header = (props: Props) => {
-  const [selectedPage, setSelectedPage] = useState("/");
-
-  const navItemsInternal = navStrings.map((string, i) => (
-    <div className={navRoutes[i] === selectedPage ? "nav-item nav-item-selected" : "nav-item"} key={`nav-item${i}`}>
-      <Link to={navRoutes[i]} className={"nav-link"} onClick={() => setSelectedPage(navRoutes[i])}>
-        {string}
-      </Link>
-    </div>
-  ));
-
-  const documentStrings = ["Resume", "Transcript", "Cover Letter", "Letter of Recommendation"];
-
   const menuProps = useConst<IContextualMenuProps>(() => ({
     shouldFocusOnMount: true,
     items: documentStrings.map((document) => ({
@@ -32,7 +20,7 @@ const Header = (props: Props) => {
   return (
     <header className="app-header">
       <Link to="/" className="logo-nav-item">
-        <img src="./CatamaranLogo.png" className={"nav-logo"} alt={"Eli Forbes"} />
+        <img src="./CatamaranLogoWhite.png" className={"nav-logo"} alt={"Eli Forbes"} />
       </Link>
       <div className="document-menu-button">
         <DefaultButton text={"Documents"} menuProps={menuProps} />
