adf26b9f0da286e2fe2b9dd9525c63737f375fa5
Eli Forbes <eliforbes42@gmail.com>
2020-10-02 11:00:48 -0700

    Overview Pane Transition on Hide

--- src/components/About/Overview.tsx
+++ src/components/About/Overview.tsx
@@ -9,29 +9,37 @@ const Overview = (props: Props) => {
   const toggleExpansion = () => setIsExpanded(!isExpanded);
 
   return isExpanded ? (
-    <div className={"profile-picture-container"}>
-      <img className={"profile-picture"} src={profilePicture} alt="Eli Forbes" />
-      <div>
-        <div className={"description-title"}>Overview</div>
-        <div className={"description-text"}>
-          Eli is a Software Engineer with a passion for creating smooth visual experiences, a keen interest in learning
-          various technologies, and a love for coding in general.
+    <div>
+      <div className={"profile-picture-container"}>
+        <img className={"profile-picture"} src={profilePicture} alt="Eli Forbes" />
+        <div>
+          <div className={"description-title"}>Overview</div>
+          <div className={"description-text"}>
+            Eli is a Software Engineer with a passion for creating smooth visual experiences, a keen interest in
+            learning various technologies, and a love for coding in general.
+          </div>
         </div>
+        <FontIcon
+          iconName={"DoubleChevronLeft8"}
+          className={"fabric-logo expansion-chevron footer-link"}
+          onClick={toggleExpansion}
+        />
       </div>
-      <FontIcon
-        iconName={"DoubleChevronLeft8"}
-        className={"fabric-logo expansion-chevron footer-link"}
-        onClick={toggleExpansion}
-      />
+      <div className={"profile-picture-container"} />
     </div>
   ) : (
-    <div className={"profile-picture-container-collapsed"}>
-      <img className={"invisible"} src={profilePicture} alt="Eli Forbes" />
-      <FontIcon
-        iconName={"DoubleChevronRight8"}
-        className={"fabric-logo expansion-chevron footer-link"}
-        onClick={toggleExpansion}
-      />
+    <div>
+      <div className={"profile-picture-container-collapsed"}>
+        <img className={"invisible"} src={profilePicture} alt="Eli Forbes" />
+        <FontIcon
+          iconName={"DoubleChevronRight8"}
+          className={"fabric-logo expansion-chevron footer-link"}
+          onClick={toggleExpansion}
+        />
+      </div>
+      <div className={"profile-picture-container-collapsed"}>
+        <FontIcon iconName={"DoubleChevronRight8"} className={"fabric-logo expansion-chevron footer-link invisible"} />
+      </div>
     </div>
   );
 };
