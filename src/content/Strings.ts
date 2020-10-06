1e3a8057766605df1fe2f4e6cb41811963382b2b
Eli Forbes <eliforbes42@gmail.com>
2020-10-06 15:09:46 -0700

    Background content additions

--- src/content/Strings.ts
+++ src/content/Strings.ts
@@ -37,18 +37,16 @@ export const aboutContent = {
   background: {
     title: "Background",
     background: [
-      `At a young age, Eli took an interest in graphic design, using tools such as GIMP, Photoshop, and
-    Illustrator. He soon created a gaming-related blog to improve upon his graphic design skills, 
-    and market such skills to like-minded peers in the game's community. During this time, he learned of HTML, CSS, and eventually JavaScript. 
-    Upon these discoveries, he took a strong interest in the creation of visual experiences using such languages in combination with his graphic design efforts,
-    and began progressively improved his blog during his learning about web development.`,
+      `At a young age, Eli took an interest in graphic design, using tools such as GIMP, Photoshop, and Illustrator. 
+      He soon created a gaming-related blog to improve upon his graphic design skills and put them to use for others within the game's community. 
+      During this time, he learned of HTML, CSS, and eventually JavaScript, and took a strong interest in the creation of visual experiences using such languages in combination with his graphic design efforts.
+      He took great pleasure in the challenge of learning and utilizing each language in conjunction with another, as it was like an intricate puzzle with an infinite number of possible end results.`,
 
-      `During Middle School and High School, while general studies turned to occupy the bulk of his time, 
-      he still maintained an interest in graphic design. In High School he got more into programming by learning the basics of coding
-    in C and Java, for use in the Robotics Club. This learning sparked a larger interest in coding, as well as the motivation for Eli to move into
+      `During Middle School and High School, while general studies turned to occupy the bulk of his time, he continued to maintain an interest in graphic design. 
+      In High School he got further into programming by learning the basics of coding in C and Java, for use in the Robotics Club. 
+      This learning sparked a larger interest in coding, as well as the motivation for Eli to move into
     University and dive head-first into pursuing a Computer Science degree. He progressively became proficient in C, C++, and
-    C#, while gaining experience in other languages such as SQL, Java, and Python. He strove to learn as much as possible, 
-    gaining experience in Containerization through Docker and Kubernetes, as well as learning of Cloud Computing through Google Cloud Platform and Skytap Cloud. 
+    C#, while gaining experience in other languages such as SQL, Java, and Python. 
     During this time, C became his strong suit, as he soon became a Teaching Assistant for the introductory programming class, and
     eventually utilized the language to create both a File System and Operating System.`,
 
@@ -59,12 +57,20 @@ export const aboutContent = {
     Android Studio. While they weren't pictures of perfection by any means, he thoroughly enjoyed the challenge
     and learning experience that each iteration provided.`,
 
-      `To this day, Eli continues to enjoy each and every challenge of being a Software Engineer, as thus far he's
-    had the pleasure of turning his hobby into a profession, and especially since there's always something new
+      `Eli found his internship at CDK Global to be an amazing opportunity, as it was the beginning of turning his hobby into a profession! 
+    He thoroughly enjoyed using his cummulative web development experience every day, and endeavored to continuously build on top of it with new learnings.
+    Upon conclusion of the internship, Eli was resolved to become a full time web developer.`,
+
+      `During the remaining time at University, he strove to learn as much as possible through his coursework, as well as his work in various group projects and clubs. 
+    Some courses further exposed him to multiple other programming languages, both old and new. 
+    While others provided him with experience in various newer technlogogies, such as Containerization through Docker and Kubernetes, and exposure to Cloud Computing through both Google Cloud Platform and Skytap Cloud.
+    In addition to all the various technical learnings, participation in group projects and clubs provided continuous opportunities for growth in many softer skills such as communication, teamwork, flexibility, and much more.`,
+
+      `To this day, Eli dilligently pursues his various hobbies, and continues to enjoy each and every challenge of being a Software Engineer, as there's always something new
     to learn..`,
     ],
   },
-  hobbies: { title: "Hobbies", hobbies: ["Graphic Design", "Music Production", "Strategy Games"] },
+  hobbies: { title: "Hobbies", hobbies: ["Graphic Design", "Music Production", "Strategy Games", "Longboarding"] },
 };
 export const overviewContent = {
   title: "Overview",
